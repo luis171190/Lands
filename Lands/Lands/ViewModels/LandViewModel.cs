acd06424b8360a336f3c9b6448719dd77e89cd8b
Luis Molina <luis_03_06_2@hotmail.com>
2018-05-04 22:54:23 -0300

    Borders page

--- Lands/Lands/ViewModels/LandViewModel.cs
+++ Lands/Lands/ViewModels/LandViewModel.cs
@@ -1,8 +1,22 @@
 ﻿namespace Lands.ViewModels
 {
     using Models;
-    public class LandViewModel
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    public class LandViewModel : BaseViewModel
     {
+        #region Attributes
+        private ObservableCollection<Border> borders;
+
+        public ObservableCollection<Border> Borders
+        {
+            get { return this.borders; }
+            set { this.SetValue(ref this.borders, value); }
+        }
+        #endregion
+
         #region Propperties
         public Land Land { get; set; }
         #endregion
@@ -11,6 +25,28 @@
         public LandViewModel(Land land)
         {
             this.Land = land;
+            this.LoadBorders();
+        }
+        #endregion
+
+        #region Methods
+        private void LoadBorders()
+        {
+            this.Borders = new ObservableCollection<Border>();
+            foreach (var border in this.Land.Borders)
+            {
+                var land = MainViewModel.GetInstance().LandsList.
+                                        Where(l => l.Alpha3Code == border).
+                                        FirstOrDefault();
+                if(land != null)
+                {
+                    this.Borders.Add(new Border
+                    {
+                        Code = land.Alpha3Code,
+                        Name = land.Name,
+                    });
+                }
+            }
         }
         #endregion
     }
