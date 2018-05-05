9173583101fd6c4a6d8011023c0326d257fd1c43
Luis Molina <luis_03_06_2@hotmail.com>
2018-05-04 23:22:34 -0300

    Finish land page

--- Lands/Lands/ViewModels/LandViewModel.cs
+++ Lands/Lands/ViewModels/LandViewModel.cs
@@ -9,16 +9,29 @@
     {
         #region Attributes
         private ObservableCollection<Border> borders;
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
+        #endregion
 
+        #region Propperties
+        public Land Land { get; set; }
         public ObservableCollection<Border> Borders
         {
             get { return this.borders; }
             set { this.SetValue(ref this.borders, value); }
         }
-        #endregion
 
-        #region Propperties
-        public Land Land { get; set; }
+        public ObservableCollection<Currency> Currencies
+        {
+            get { return this.currencies; }
+            set { this.SetValue(ref this.currencies, value); }
+        }
+
+        public ObservableCollection<Language> Languages
+        {
+            get { return this.languages; }
+            set { this.SetValue(ref this.languages, value); }
+        }
         #endregion
 
         #region Constructors
@@ -26,6 +39,8 @@
         {
             this.Land = land;
             this.LoadBorders();
+            this.Currencies = new ObservableCollection<Currency>(this.Land.Currencies);
+            this.Languages = new ObservableCollection<Language>(this.Land.Languages);
         }
         #endregion
 
